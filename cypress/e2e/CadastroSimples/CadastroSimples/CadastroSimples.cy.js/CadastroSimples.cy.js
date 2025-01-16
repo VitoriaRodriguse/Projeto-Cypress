e16bf1841e7c12fe923590a02263f862a1d3e7aa
VitóriaRodriguse <vitoriapacheco777@gmail.com>
2025-01-16 18:34:31 -0300

    Novo repositorio

--- cypress/e2e/CadastroSimples/CadastroSimples/CadastroSimples.cy.js/CadastroSimples.cy.js
+++ cypress/e2e/CadastroSimples/CadastroSimples/CadastroSimples.cy.js/CadastroSimples.cy.js
@@ -1,30 +1,30 @@
 /// <reference types="cypress" />
-import { faker } from '@faker-js/faker';
+import { pt_BR, en, Faker } from '@faker-js/faker';
+
+export const faker = new Faker({
+    locale: [pt_BR, en],
+});
 
 describe('Cadastro Simples', () => {
 
-    let nome, email, senha, dataDeNascimento, telefone, endereço, estado, cidade
+    let nome, email, senha, genero, dataDeNascimento, telefone, endereço, estado, cidade
     
     beforeEach(() => {
         cy.visit('/projectqatesterweb/cadastro-simples.html')
         nome = faker.person.fullName();
         email = faker.internet.email();
         senha = faker.internet.password();
-        dataDeNascimento = faker.date.birthdate({ minAge: 18, maxAge: 65}).tolSOString().split('T')[0];
+        genero = faker.person.gender();
+        dataDeNascimento = faker.date.birthdate({ minAge: 18, maxAge: 65}).toISOString().split('T')[0];
         telefone = faker.phone.number('+55549########');
         endereço = faker.location.streetAddress();
+        estado = faker.location.state();
+        cidade = faker.location.city();
     })
 
     it('Submeter Formulário de Cadastro Simples', () => {
 
-        cy.get('#name').should('be.visible').and('have.value', '').type(nome, { force: true});
-        cy.get('#email').should('be.visible').and('have.value', '').type(email, { force: true});
-        cy.get('#password').should('be.visible').and('have.value', '').type(senha, { log: false});
-        cy.get('#birthdate').should('be.visible').and('have.value', '').type(dataDeNascimento);
-        cy.get('#phone').should('be.visible').and('have.value', '').type(telefone);
-        cy.get('#address').should('be.visible').and('have.value', '').type(endereço);
-        cy.get('#state').should('be.visible').and('have.value', '').select('Rio Grande do Sul');
-        cy.get('#city').should('be.visible').type('Gramado');
+        cy.submitForms(nome, email, senha, genero, dataDeNascimento, telefone, endereço, estado, cidade);
         cy.get('#terms').should('be.visible').check();
         cy.get('#cadastroForm > button').should('not.be.disabled').click();
         cy.get('#alertMessage').should('contain', 'Cadastro realizado com sucesso!');
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,13 @@
-Cypress.Commands.add('submitForms', (nome, email, senha, dataDeNascimento, telefone, endereço, estado, cidade) => {
+Cypress.Commands.add('submitForms', (nome, email, senha, genero, dataDeNascimento, telefone, endereço, estado, cidade) => {
 
-    
+    cy.get('#name').should('be.visible').and('have.value', '').type(nome, { force: true});
+        cy.get('#email').should('be.visible').and('have.value', '').type(email, { force: true});
+        cy.get('#password').should('be.visible').and('have.value', '').type(senha, { log: false});
+        cy.get('#gender').should('be.visible').type(genero);
+        cy.get('#birthdate').should('be.visible').and('have.value', '').type(dataDeNascimento);
+        cy.get('#phone').should('be.visible').and('have.value', '').type(telefone);
+        cy.get('#address').should('be.visible').and('have.value', '').type(endereço);
+        cy.get('#state').should('be.visible').and('have.value', '').select(estado);
+        cy.get('#city').should('be.visible').type(cidade);
 
 })
